daa84d5a1277e90b23d0690f727188823938fdd4
SANIAT INJAM <saniat.cse.ewu@gmail.com>
2025-03-29 06:26:12 +0600

    device preview added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:world_time_app/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(builder: (context) => MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: HomeScreen());
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+
+      title: 'Flutter Demo',
+      home: HomeScreen(),
+    );
   }
 }
